4f15087dc083956efa8c93aac5d2c8219952e487
Shjsx2 <s4jin@ucsd.edu>
2021-02-18 15:22:45 -0800

    Implement save and dropdown menu

--- js/createWall.js
+++ js/createWall.js
@@ -1,17 +1,34 @@
 $(document).ready(function () {
-    all_notes = $("li a");
     addButton = $("#addButton");
+    saveButton = $("#save")
     back = $("#back");
-    text =$("#textBox");
+    text = $("#textBox");
     addButton.click(addNote);
     back.click(goPrevious);
+    saveButton.click(onSaveClick)
   });
 
-
 function addNote() {
-  $("body ul").append("<li><a href='#' contenteditable='true'><h2>New Note</h2><p>type something...</p></a></li>")
+  var colors = ["#c4c4c4", "#f2f2f2", "#000000"]
+  var c = colors[Math.floor(Math.random()*colors.length)]
+  $("body ul").append("<li><a href='#' contenteditable='true'><h2>New Note</h2><p>type something...</p></a></li>");
+}
+
+function onMoreClick() {
+  $("#moreDropdown").toggle('show')
 }
 
 function goPrevious(){
-  history.go(-1);
+  history.go('../index.html');
+}
+
+function onSaveClick() {
+  var notes = $('.container li a')
+  var titles =[];
+  var notes = [];
+  $('.container li a').each(function(idx){
+    titles.push($(this).find('h1').text())
+    notes.push($(this).find('p').text())
+  })
+  window.localStorage.setItem('titles', titles.)
 }
