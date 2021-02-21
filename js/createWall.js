414bf61984acdc27d1c9838eea32cb53a056e86f
Shjsx2 <s4jin@ucsd.edu>
2021-02-20 23:46:06 -0800

    implemented local storage

--- js/createWall.js
+++ js/createWall.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function () {
     addButton = $("#addButton");
     saveButton = $("#save")
@@ -6,18 +8,30 @@ $(document).ready(function () {
     addButton.click(addNote);
     back.click(goPrevious);
     saveButton.click(onSaveClick)
+
+    notes = JSON.parse(window.localStorage.getItem('notes'));
+    notes.forEach(function(note) {
+      addNote(null, note.title, note.text)
+    })
+    $('.note').on('focus', function(e){
+      $(this).parent().attr('draggable', 'false')
+    })
+
+    $('.note').on('blur', function(e){
+      $(this).parent().attr('draggable', 'true')
+    })
   });
 
   
-$( function() {
-  $(".note").draggable({
-      cursor: "grabbing",
-      opacity: 0.5,
-      //grid:[300,300],
-      snap: true,
-      snapTolerance: 30
-  });
-} );
+// $( function() {
+//   $(".note").draggable({
+//       cursor: "grabbing",
+//       opacity: 0.5,
+//       //grid:[300,300],
+//       snap: true,
+//       snapTolerance: 30
+//   });
+// } );
 
 // function addNote() {s
 
@@ -29,39 +43,12 @@ $( function() {
 // }
 /*"<li class = 'note ui-draggable ui-draggable-handle' style='position: relative; opacity: 1; >> <a href='#' contenteditable='true'><h2>New Note</h2><p>type something...</p></a></li>"*/
 
-function addNote() {
+function addNote(e, title='new note', text='type something....') {
   // todo: give random colors to new note
-  
+  console.log("loading "+title)
   var colors = ["#c4c4c4", "#f2f2f2", "#000000"]
   var c = colors[Math.floor(Math.random()*colors.length)]
-  $("body ul").append('<li class="note"> <a href="#" contenteditable="true"> <h2>New Note</h2> <p>type something...</p></a></li>');
-  $(".note").draggable({
-    cursor: "grabbing",
-    opacity: 0.5,
-    //grid:[300,300],
-    snap: true,
-    snapTolerance: 30
-});
-}
-
-function savePage(){
-  //select content in body, add that to a new page and connect that to gallery
-  //or duplicates file to "userviews"
-  //connected to save button
-
-  var bod = $("#wallBod");
- // const fs = require('fs');
-  var file = new File(bod, "newfile2.handlebars",{
-    type: "hadlebars",
-  })
-
-  $()
-  // fs.writeFile('newFile1.handlebars', 'test content', function(err){
-  //   if(err) throw err;
-  //   console.log('file saved!');
-  // })
-
-  console.log('file saved!');
+  $("body ul").append('<li class="note"> <a href="#" contenteditable="true"><h2>'+title+'</h2><p>'+text+'</p></a></li>');
 }
 
 function onMoreClick() {
@@ -75,11 +62,12 @@ function goPrevious(){
 function onSaveClick() {
   // todo: give every note an unique id and save
   var notes = $('.container li a')
-  var titles =[];
-  var notes = [];
+  var notes = []
   $('.container li a').each(function(idx){
-    titles.push($(this).find('h1').text())
-    notes.push($(this).find('p').text())
+    var title = $(this).find('h2').text()
+    var text = $(this).find('p').text()
+    notes.push({'title': title, 'text': text})
   })
+  window.localStorage.setItem('notes', JSON.stringify(notes))
 }
 
